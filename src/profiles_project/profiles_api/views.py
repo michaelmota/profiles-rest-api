ed5019485db64ed65d1b64de903a86cc43171e8d
Michael Mota <michael@alternadr.com>
2017-10-18 23:11:44 -0400

    Added login API.

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -6,6 +6,8 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from . import serializers
 from . import models
@@ -114,4 +116,14 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 	authentication_classes = (TokenAuthentication,)
 	permission_classes = (permissions.UpdateOwnProfile,)
 	filter_backends = (filters.SearchFilter,)
-	search_fields = ('name', 'email')
+	search_fields = ('name', 'email',)
+
+class LoginViewSet(viewsets.ViewSet):
+	"""Check emails and password and returns an auth token."""
+
+	serializer_class = AuthTokenSerializer
+
+	def create(self,request):
+		"""Use the obtainauthtoken apiview to validate and create a token."""
+
+		return ObtainAuthToken().post(request)
